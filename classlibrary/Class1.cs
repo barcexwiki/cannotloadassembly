bb422691251646e3d337a9aaa7f7673d4a2f9fcf
barcex <barcexwiki@gmail.com>
2017-07-06 12:51:49 +0200

    first commit

---
+++ classlibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classlibrary
+{
+    public class Class1
+    {
+        public static string HelloWorld()
+        {
+            return "Hello World from classlibrary!";
+        }
+    }
+}
---
+++ psmodule/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace psmodule
+{
+    [Cmdlet(VerbsCommon.Get, "HelloWorld")]
+    public class GetHelloWorld : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            WriteObject(classlibrary.Class1.HelloWorld());
+        }
+    }
+}
